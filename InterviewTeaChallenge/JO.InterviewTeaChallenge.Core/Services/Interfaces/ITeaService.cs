5dc9d0a3ae61a84934135ad982c765693129e11d
James O'Neill <gingeoneill@gmail.com>
2017-11-09 21:04:18 +0000

    Added Tea Service and interface

--- InterviewTeaChallenge/JO.InterviewTeaChallenge.Core/Services/Interfaces/ITeaService.cs
+++ InterviewTeaChallenge/JO.InterviewTeaChallenge.Core/Services/Interfaces/ITeaService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using JO.InterviewTeaChallenge.Data.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,37 @@ namespace JO.InterviewTeaChallenge.Core.Services.Interfaces
 {
     public interface ITeaService
     {
+        /// <summary>
+        /// Creates a new tea
+        /// </summary>
+        /// <param name="tea">The tea instance</param>
+        /// <returns>The ID of the new tea</returns>
+        int CreateTea(Tea tea);
+
+        /// <summary>
+        /// Deletes the tea with the given ID
+        /// </summary>
+        /// <param name="id">The ID of the tea to delete</param>
+        void DeleteTea(int id);
+
+        /// <summary>
+        /// Gets the tea with the given ID
+        /// </summary>
+        /// <param name="id">The ID of the tea</param>
+        /// <returns>The tea instance</returns>
+        Tea GetTea(int id);
+
+        /// <summary>
+        /// Gets the set of teas
+        /// </summary>
+        /// <returns>The set of teas</returns>
+        IReadOnlyCollection<Tea> GetTeas();
+
+        /// <summary>
+        /// Updates the given tea
+        /// </summary>
+        /// <param name="id">The ID of the tea</param>
+        /// <param name="tea">The updated tea</param>
+        void UpdateTea(int id, Tea tea);
     }
 }
--- InterviewTeaChallenge/JO.InterviewTeaChallenge.Core/Services/TeaService.cs
+++ InterviewTeaChallenge/JO.InterviewTeaChallenge.Core/Services/TeaService.cs
@@ -2,10 +2,35 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using JO.InterviewTeaChallenge.Data.Models;
 
 namespace JO.InterviewTeaChallenge.Core.Services
 {
     public class TeaService : ITeaService
     {
+        public int CreateTea(Tea tea)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteTea(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Tea GetTea(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReadOnlyCollection<Tea> GetTeas()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateTea(int id, Tea tea)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
